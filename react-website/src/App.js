b318479b8c58e21a0d72baf53025d1aba59c4a62
kevinkzx <kevinkuerzx@gmail.com>
2021-03-08 00:08:18 +0800

    second branch of nav bar

--- react-website/src/App.js
+++ react-website/src/App.js
@@ -5,9 +5,9 @@ import {BrowserRouter as Router} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Navbar />
-    </div>
+    </Router>
   );
 }
 
