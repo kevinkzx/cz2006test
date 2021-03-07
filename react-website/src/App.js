6abb353cdace4ed047f2c893f45429c48246d05e
kevinkzx <kevinkuerzx@gmail.com>
2021-03-07 15:43:00 +0800

    react folder

---
+++ react-website/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Whats's upppppp</h1>
+    </div>
+  );
+}
+
+export default App;
