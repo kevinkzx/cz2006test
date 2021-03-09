07008c690a3e1be9c86d722a66798fd757a2d34a
Eugene Hoe <eugenehoe19@gmail.com>
2021-03-10 01:47:33 +0800

    Edited image name

--- react-website/src/components/Services/index.js
+++ react-website/src/components/Services/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Icon1 from '../../images/svg-1.svg';
+import Icon1 from '../../images/svg-3.svg';
 import Icon2 from '../../images/svg-2.svg';
 import Icon3 from '../../images/svg-5.svg';
 import {ServicesContainer, ServicesH1, ServicesWrapper, ServicesCard, ServicesIcon, ServicesH2, ServicesP} from './ServicesElements';
